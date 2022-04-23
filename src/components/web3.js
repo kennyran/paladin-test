742d3c276f708c1fbfde3bc5d5f5d5f0eaf996e3
Kenny Rancour <rancourkenny@gmail.com>
2022-04-23 15:13:14 -0400

    new commit

--- src/components/web3.js
+++ src/components/web3.js
@@ -1,5 +1,5 @@
 const Web3 = require('web3')
 
-const web3 = new Web3(new Web3.providers.HttpProvider('https://mainnet.infura.io/v3/138a070a8c734e448e46beaa2c244861'))
+const web3 = new Web3(new Web3.providers.HttpProvider('https://mainnet.infura.io/v3/...'))
 
 module.exports = web3;
